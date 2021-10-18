e957300d50371e702c92491518d12c4d352612ca
Vinicius Resende <viniatresnde@gmail.com>
2021-10-18 10:46:19 -0300

    Melhora uso de memória e fusion com função simplify

--- Pops/Populational.hs
+++ Pops/Populational.hs
@@ -8,14 +8,11 @@ module Pops.Populational (
   executeAlgorithm,
   parExecuteAlgorithm
  )where
-import GHC.Word (Word64)
 import Pops.Solution
 import Pops.Rng
 import System.Random
 import Control.Monad.State.Strict
-import Control.DeepSeq (force)
 import Control.Parallel.Strategies
-import Data.Tree (flatten)
 
 
 -- A modification localized to a single solution
@@ -33,6 +30,24 @@ data Populational s where
   Select :: (Solution s) => Selector s -> Populational s -> Populational s -> Populational s
   IndMod :: (Solution s) => IndividualModifier s -> Populational s -> Populational s
   End :: Populational s
+
+
+simplify :: Populational s -> Populational s
+simplify End = End
+
+simplify (Select sel algo1 algo2) = Select sel (simplify algo1) (simplify algo2)
+
+simplify (PopMod mod pop) =
+  case pop of
+    PopMod _ _ -> let PopMod mod' pop' = simplify pop
+                  in PopMod (mod >=> mod') pop'
+    _          -> PopMod mod (simplify pop)
+
+simplify (IndMod mod pop) =
+  case pop of
+    IndMod _ _ -> let IndMod mod' pop' = simplify pop
+                  in IndMod (mod >=> mod') pop'
+    _          -> IndMod mod (simplify pop)
 
 step :: Solution s => Populational s -> [s] -> Rng [s]
 step End pop = return pop
@@ -70,6 +85,7 @@ parStep (Select sel op1 op2) pop = do
 executeAlgorithm :: (Solution s) => Int -> Int -> Int -> Populational s -> [s]
 executeAlgorithm seed size iter algo = evalState (exec algo initPop 0) gen
   where
+    algo' = simplify algo
     gen = mkStdGen seed
     initPop = evalState (replicateM size createRandom) gen
     exec algo pop currIter
@@ -82,6 +98,7 @@ executeAlgorithm seed size iter algo = evalState (exec algo initPop 0) gen
 parExecuteAlgorithm :: (Solution s, NFData s) => Int -> Int -> Int -> Populational s -> [s]
 parExecuteAlgorithm seed size iter algo = evalState (exec algo initPop 0) gen
   where
+    algo' = simplify algo
     gen = mkStdGen seed
     initPop = evalState (replicateM size createRandom) gen
     exec algo pop currIter
